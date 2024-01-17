02a2e6f826449d7b5989e035532c3aa56fbdc9f3
morinco <y.pinkberry@gmail.com>
2024-01-17 19:03:26 +0900

    xx

---
+++ myjs/test.js
@@ -0,0 +1,14 @@
+function checkSequence(event) {
+  
+  const key = event.key;
+  sessionStorage["key"] = sessionStorage["key"] + key;
+  var str = sessionStorage["key"];
+
+  // メッセージボックスを表示
+  if (str.includes('ArrowUpArrowUpArrowDownArrowDownArrowRightArrowLeftab')) {
+    sessionStorage["key"] = "";
+    alert(`イースターエッグ！`);
+  } else {
+  }
+}
+
